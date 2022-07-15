d5b81dae3641045c96cba9a49a3696879d7d5281
takekou0130 <60650456+takekou0130@users.noreply.github.com>
2022-07-16 01:13:59 +0900

    add problems

---
+++ abc-problems/78/C/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld ;
+typedef pair<ll,ll> P ;
+typedef tuple<ll,ll,ll> TP ;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define endl "\n"
+#define chmin(a,b) a = min(a,b)
+#define chmax(a,b) a = max(a,b)
+#define bit_count(x) __builtin_popcountll(x)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) a / gcd(a,b) * b
+
+ll n,m;
+int main(){
+  cin >> n >> m;
+  ll base = 1900*m+100*(n-m);
+  ll ni = 1;
+  rep(i, m) ni *= 2;
+  cout << base * ni << endl;
+}
